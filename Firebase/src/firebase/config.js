6025e6122c821ba74d28558dd8a922f4a720ae17
Aayush Jain <jaayush014@gmail.com>
2020-12-28 02:44:25 +0530

    firebase added

---
+++ Firebase/src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: 'AIzaSyCGpaI-JJmgRGKLxC3OXXzW7YM-a8E_2Qk',
+  authDomain: 'confusion-ee311.firebaseapp.com',
+  projectId: 'confusion-ee311',
+  storageBucket: 'confusion-ee311.appspot.com',
+  messagingSenderId: '963323452897',
+  appId: '1:963323452897:web:a5a316f12a385da52606a0',
+  measurementId: 'G-J43HJ4ZJE4',
+};
